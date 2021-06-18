b10467bfa841ac118681c274b8cb901a055ccf43
sailinux24 <61447774+sailinux24@users.noreply.github.com>
2021-06-18 09:14:49 +0530

    comit

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Guys this demo of build config</p>'; ?> 
+ <?php echo '<p>Hello Guys this demo of build config   updated the code with lastest changes</p>'; ?> 
  </body>
 </html>
